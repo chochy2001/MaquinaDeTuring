f6e8b268a065dbbbc78b30f313cf99e0aee724cc
Jorge Salgado Miranda <ohchochy@gmail,com>
2022-05-13 17:14:33 -0500

    Se logró finalizar con éxito el programa, faltan hacer ajustes para una mejor experiencia con numero mayores a 9 y simbolos repetidos

--- main.c
+++ main.c
@@ -5,18 +5,19 @@
 #include "conversionBinario.h"
 #include "multiplicacionBinario.h"
 #include "conversionEntero.h"
+#include "validacionEntrada.h"
 
 static long arregloBinario[100];
-long contador = 0;
+int contador = 0, contadorAux = 0;
 long multiplicacionRecursiva( long a, long b);
 
 int main() {
     //todo hacer maquina de turing para verificar que es correcta la entrada.
     //todo diseñar la multiplicación de binarios con respecto a el retorno de conversiónBinario.h
 
-    bool sesion = true;
-    char entrada[20] = "", simbolo[] = "x";
-    int arregloEnteros[100];
+    bool sesion = true,validacionCadena = true;
+    char entrada[20], simbolo[] = "x",resultadoString[100] = "",entradaFinal[100] = "";
+    int arregloEnteros[100],continuar;
     long resultado = 0;
     printf("Este programa realiza una multiplicaci%cn de dos n%cmeros naturales a trav%cs ", 162, 163, 130);
     printf("del algoritmo de Maquina de Turing\n");
@@ -27,97 +28,69 @@ int main() {
 
     //todo diseñar la maquina de Turing para que antes de evaluar
     // los números verifique si la entrada es correcta
-    while (sesion) {
-        printf("Ingrese la multiplicaci%cn que desea realizar\n", 162);
-        scanf("%s", entrada);
-        char *token = strtok(entrada, simbolo);
-        printf("Numero = %s\n", token);
-        arregloEnteros[contador] = atoi(token);
-        arregloBinario[contador] = conversionBinario(atoi(token));
-        contador++;
 
-        while ((token = strtok(NULL, simbolo)) != NULL) {
+        //todo verificar que la entrada sea correcta aquí
+        //todo si la entrada no es correcta no se debe pasar de aqui.
+        do{
+            contador = 0;
+            contadorAux = 0;
+            strcpy(entradaFinal,"");
+            printf("Ingrese la multiplicaci%cn que desea realizar\n", 162);
+            fflush(stdin);
+            scanf("%s", entrada);
+            strcat(entradaFinal, entrada);
+            if(validacionEntrada(entrada, strlen(entrada)) == -1){
+                printf("La entrada no es correcta\n");
+                validacionCadena = true;
+                continue;
+            }
+            char *token = strtok(entrada, simbolo);
             printf("Numero = %s\n", token);
-            //todo transformarlo a binario
-            //todo almacenamos en el arreglo (si lo usamos)
             arregloEnteros[contador] = atoi(token);
             arregloBinario[contador] = conversionBinario(atoi(token));
             contador++;
-        }
-        for (int i = 0; i < contador/*longitud arreglo*/; i++) {
-            printf("Binario [ %ld ] (%ld) = %ld\n", i + 1, arregloEnteros[i], arregloBinario[i]);
-        }
+
+            while ((token = strtok(NULL, simbolo)) != NULL) {
+                printf("Numero = %s\n", token);
+                //todo transformarlo a binario
+                //todo almacenamos en el arreglo (si lo usamos)
+                arregloEnteros[contador] = atoi(token);
+                arregloBinario[contador] = conversionBinario(atoi(token));
+                contador++;
+            }
+            for (int i = 0; i < contador/*longitud arreglo*/; i++) {
+                printf("Binario [ %ld ] (%ld) = %ld\n", i + 1, arregloEnteros[i], arregloBinario[i]);
+            }
+
+            resultado = multiplicacionRecursiva(0,0);
+            sprintf(resultadoString, "%ld", resultado);
+            if(validacionCadena == true){
+                printf("\nEl resultado de la multiplicacion de %s = %ld = %ld\n",entradaFinal,resultado,conversionEntero(resultadoString, strlen(resultadoString)));
+            }
 
 
-        // Todo prueba de multiplicación binaria.
-        printf("\n%dx%d = %ld",arregloEnteros[0],arregloEnteros[1],multiplicacionBinario(arregloBinario[0],arregloBinario[1]));
-        //todo prueba de conversión de binario a decimal
-        /*
-         * Como la función recibe una cadena hay que
-         * convertir el número long a una cadena de
-         * caracteres usando sprintf()
-         */
-        char numeroBinarioFinal[100];
-        sprintf(numeroBinarioFinal,"%d",multiplicacionBinario(arregloBinario[0],arregloBinario[1]));
-        printf("\nEl resultado de la multiplicacion de %ldx%ld = %s = %ld\n",arregloEnteros[0],arregloEnteros[1],numeroBinarioFinal,conversionEntero(numeroBinarioFinal, strlen(numeroBinarioFinal)));
-
-
-        // todo Multiplicarlo
-        for (int i = 0; i < contador; ++i) {
-            //todo mandar por parejas los datos del arreglo
             /*
-             * num1 = a[0] = 101
-             * num2 = a[1] = 111
-             * arreglo[]= funcionMultiplicación(a[i],a[i++]); // no c puede con impares
-             * num1  = return multiplicacion
-             * num2 = a[2];
-             *
-            */
-        }
+             * Como la función recibe una cadena hay que
+             * convertir el número long a una cadena de
+             * caracteres usando sprintf()
+             */
+            printf("Desea continuar?\n1) Si\n2) No\n");
+            scanf("%d",&continuar);
+            if(continuar == 2){
+                validacionCadena = false;
+                printf("Gracias por usar el Programa\n");
+            }
 
-        resultado = multiplicacionRecursiva(0,0);
-        printf("Resultado = %ld\n" , resultado);
-        sesion = false;
+        }while(validacionCadena == true);
+
         return 0;
     }
 
-}
 
 
 
-/*
- * multiplicación(a,b){
- *
- *
- *
- * if(contador == 0){
- *      a = arreglo[contador]      //(global)
- *      b = arreglo[contador++]    //(global)
- *      a = a x b; // llamada a la función de multiplicar
- *      multiplicación(a, arreglo[contador++]);
- * }else{
- *      a = a x b;
- * }
- *
- *
- *
- * if(contador != lim){
- *
- *      multiplicación(a, arreglo[contador++]);
- *                     // a = resultado anterior
- *
- * }
- * else{
- *
- *      return a;
- * }
- *
- *
- *
- *}
- * */
 
-int contadorAux = 0;
+
 
 long multiplicacionRecursiva(long a, long b){
     //printf("contadorAux = %d\n",contadorAux);
@@ -130,14 +103,7 @@ long multiplicacionRecursiva(long a, long b){
     }
     else {
         if(contadorAux == contador){
-            printf("Valor de a = %ld \n", a);
-            /*
-             * printf("Entra 1 \n");
-            printf("Valor de a = %ld \n", a);
-            a = multiplicacionBinario(a,b);
-            multiplicacionRecursiva(a, arregloBinario[contadorAux-1]);
-            contadorAux+=1;
-             */
+            //printf("Valor de a = %ld \n", a);
             return a;
         }
         else if( contadorAux < contador ){
--- validacionEntrada.h
+++ validacionEntrada.h
@@ -2,8 +2,54 @@
 #ifndef PROYECTOLFYA_VALIDACIONENTRADA_H
 #define PROYECTOLFYA_VALIDACIONENTRADA_H
 
+// Funciones de validacion de entrada
+//si la entrada es correcta regresa un 0 y si es incorrecta regresa un -1
+//3x2x5x3x2x1x3
+//siempre que la entrada es correcta es un numero impar.
+// Si la entrada es incorrecta de entrada es un numero para (longitud)
 
+#include <stdio.h>
 
+int contChar = 0;
+int validacionEntrada(const char entrada[], int longitud){
+    int contadorX = 0,contadorNum = 0;
+    if(entrada[0] == 'x' || entrada[longitud-1]== 'x'){
+        return -1;//si la entrada es incorrecta
+    }else{
+        for(int i = 0; i < longitud; i++){
+            if(entrada[i] == 'x'){
+                contadorX++;
+            }
+            if(entrada[i] >= '0' && entrada[i] <= '9'){
+                contadorNum++;
+            }
+        }
+        if(contadorX >= contadorNum){
+            return -1;//si la entrada es incorrecta
+        }
+        if(contadorNum > contadorX && contadorNum == 1){
+            return -1;
+        }
+        if(contadorNum == contadorX){
+            return -1;
+        }
 
+    }
+    return 0;//si la entrada es correcta regresa un 0
+}
+
+/*int estado0(const char entrada[]){
+    if(entrada[contChar] >= '0' && entrada[contChar] <= '9'){
+        contChar+=1;
+        estado1(entrada);
+    }
+    else{
+        return -1;
+    }
+}*/
+
+int estado1(){
+    
+}
 
 #endif //PROYECTOLFYA_VALIDACIONENTRADA_H
